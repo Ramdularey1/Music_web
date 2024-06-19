93d131f0c50705afc3bd2cea59677771d03ed34a
Ramdularey1 <ramdulareyyadav2@gmail.com>
2024-06-19 13:03:29 +0530

    add : test route

--- backend/router/user.routes.js
+++ backend/router/user.routes.js
@@ -5,6 +5,11 @@ import { addMusic,getMusic } from "../controller/music.controller.js";
 
 const router = Router();
 
+router.route("/test").get((_,res) => {
+    return res.json({
+        success : true
+    })
+})
 router.route("/register").post(registerUser)
 router.route("/login").post(loginUser)
 router.route("/logout").post(verifyJWT, logoutUser)
