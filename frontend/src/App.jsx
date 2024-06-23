0acb86f965ddc6f8a62cadcd0e096689bbcd7fa0
Ramdularey1 <ramdulareyyadav2@gmail.com>
2024-06-24 00:08:29 +0530

    test1

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,37 +3,37 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Provider, useSelector } from "react-redux";
 import store from "./utils/store";
-import NavBar from "./Component/NavBar";
+// import NavBar from "./Component/NavBar";
 import Body from "./Component/Body";
 
-import AllMusic from "./Component/AllMusic";
-import Footer from "./Component/Footer";
-import OldMusic from "./Component/OldMusic";
-import Trending from "./Component/Trending";
-import NewSong from "./Component/NewSong";
-import HindiSong from "./Component/HindiSong";
-import TopAlbum from "./Component/TopAlbum";
-import Type from "./Component/Type";
-import Music from "./Component/Music";
-import EnglishSongs from "./Component/EnglishSongs";
-import SpanishSong from "./Component/SpanishSong";
-import SouthSong from "./Component/SouthSong";
-import MotivationalSong from "./Component/MotivationalSong";
-import PunjabiSong from "./Component/PunjabiSong";
-import RomanticSong from "./Component/RomanticSong";
-import TopMusic from "./Component/TopMusic";
-import ArjitSingh from "./Component/carousel/ArjitSingh";
-import UditNarayan from "./Component/UditNarayan";
-import KumarSanu from "./Component/KumarSanu";
-import AbhijeetSong from "./Component/AbhijeetSong";
-import KkSong from "./Component/KkSong";
-import HoneySinghSong from "./Component/HoneySinghSong";
-import BadshahSong from "./Component/BadshaSong";
-import MusicContainer from "./Component/MusicContainer";
-import Registration from "./Component/auth/Forms/Registration";
-import AuthLayout from "./Component/auth/AuthLayout";
+// import AllMusic from "./Component/AllMusic";
+// import Footer from "./Component/Footer";
+// import OldMusic from "./Component/OldMusic";
+// import Trending from "./Component/Trending";
+// import NewSong from "./Component/NewSong";
+// import HindiSong from "./Component/HindiSong";
+// import TopAlbum from "./Component/TopAlbum";
+// import Type from "./Component/Type";
+// import Music from "./Component/Music";
+// import EnglishSongs from "./Component/EnglishSongs";
+// import SpanishSong from "./Component/SpanishSong";
+// import SouthSong from "./Component/SouthSong";
+// import MotivationalSong from "./Component/MotivationalSong";
+// import PunjabiSong from "./Component/PunjabiSong";
+// import RomanticSong from "./Component/RomanticSong";
+// import TopMusic from "./Component/TopMusic";
+// import ArjitSingh from "./Component/carousel/ArjitSingh";
+// import UditNarayan from "./Component/UditNarayan";
+// import KumarSanu from "./Component/KumarSanu";
+// import AbhijeetSong from "./Component/AbhijeetSong";
+// import KkSong from "./Component/KkSong";
+// import HoneySinghSong from "./Component/HoneySinghSong";
+// import BadshahSong from "./Component/BadshaSong";
+// import MusicContainer from "./Component/MusicContainer";
+// import Registration from "./Component/auth/Forms/Registration";
+// import AuthLayout from "./Component/auth/AuthLayout";
 import RootLayout from "./Component/root/RootLayout";
-import Login from "./Component/auth/Forms/Login";
+// import Login from "./Component/auth/Forms/Login";
 
 function App() {
   return (
@@ -42,7 +42,7 @@ function App() {
           <Routes>
             <Route element={<RootLayout/>}>
               <Route path="/" element={<Body />} />
-              <Route path="/allmusic" element={<AllMusic />} />
+              {/* <Route path="/allmusic" element={<AllMusic />} />
               <Route path="/oldmusic" element={<OldMusic />} />
               <Route path="/trendingsong" element={<Trending />} />
               <Route path="/newsong" element={<NewSong />} />
@@ -61,12 +61,12 @@ function App() {
               <Route path="/Abhijeet Bhattacharya" element={<AbhijeetSong />} />
               <Route path="/Krishnakumar Kunnath" element={<KkSong />} />
               <Route path="/Yo Yo Honey Singh" element={<HoneySinghSong />} />
-              <Route path="/Badshah" element={<BadshahSong />} />
+              <Route path="/Badshah" element={<BadshahSong />} /> */}
             </Route>
-            <Route element={<AuthLayout/>}>
+            {/* <Route element={<AuthLayout/>}>
               <Route path="/auth" element={<Registration/>} />
               <Route path="/login" element={<Login/>} />
-            </Route>
+            </Route> */}
           </Routes> 
       </BrowserRouter>
     </Provider>
