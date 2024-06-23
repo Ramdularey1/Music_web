a1475d4ab669c7a87f347871984d93b150bc4dd9
Ramdularey1 <ramdulareyyadav2@gmail.com>
2024-06-23 22:50:35 +0530

    update

--- backend/db/index.js
+++ backend/db/index.js
@@ -1,13 +1,32 @@
+// import mongoose from "mongoose";
+// import { DB_NAME } from "../constant.js";
+
+// const connectDB = async() => {
+//     try {
+//         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        
+//         console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
+//     } catch (error) {
+//         console.log("MONGO DB CONNECTION FAILED", error)
+//         process.exit(1);
+//     }
+// }
+// export default connectDB
+
 import mongoose from "mongoose";
 import { DB_NAME } from "../constant.js";
 
-const connectDB = async() => {
+const connectDB = async () => {
     try {
-        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-        console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        console.log(`\nMongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
     } catch (error) {
-        console.log("MONGO DB CONNECTION FAILED", error)
+        console.error("MONGO DB CONNECTION FAILED", error);
         process.exit(1);
     }
-}
-export default connectDB
+};
+
+export default connectDB;
