8b2e0de4c014290e072abd4b6094da50e99a9618
Ramdularey1 <ramdulareyyadav2@gmail.com>
2024-06-24 11:49:21 +0530

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,19 @@
 
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Provider, useSelector } from "react-redux";
+import { Provider} from "react-redux";
 import store from "./utils/store";
 
-import NavBar from "./Component/NavBar";
 import Body from "./Component/Body";
 
 import AllMusic from "./Component/AllMusic";
-import Footer from "./Component/Footer";
+
 import OldMusic from "./Component/OldMusic";
 import Trending from "./Component/Trending";
 import NewSong from "./Component/NewSong";
 import HindiSong from "./Component/HindiSong";
 import TopAlbum from "./Component/TopAlbum";
-import Type from "./Component/Type";
-import Music from "./Component/Music";
+
 import EnglishSongs from "./Component/EnglishSongs";
 import SpanishSong from "./Component/SpanishSong";
 import SouthSong from "./Component/SouthSong";
@@ -30,7 +28,7 @@ import AbhijeetSong from "./Component/AbhijeetSong";
 import KkSong from "./Component/KkSong";
 import HoneySinghSong from "./Component/HoneySinghSong";
 import BadshahSong from "./Component/BadshaSong";
-import MusicContainer from "./Component/MusicContainer";
+
 import Registration from "./Component/auth/Forms/Registration";
 import AuthLayout from "./Component/auth/AuthLayout";
 import RootLayout from "./Component/root/RootLayout";
